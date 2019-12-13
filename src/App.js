3d5ec53f41212a8414e0750a3c71222feb8a4592
Naiviv <46464295+Naiviv@users.noreply.github.com>
2019-12-13 14:29:10 -0800

    Code fixed and ready

--- src/App.js
+++ src/App.js
@@ -6,23 +6,10 @@ function App() {
   return (
     <div className="App">
     
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Web 3 Framework
-        </a>
-      </header>
     </div>
   );
 }
+
 class MemeGenerator extends React.Component {
     constructor() {
         super()
