91fbf189f3b04db6b098eaa19c47e7fb67c15f80
Naiviv <46464295+Naiviv@users.noreply.github.com>
2019-12-13 14:26:20 -0800

    fixed now?

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg.ico';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <head>
-    <title> Meme Generator </title>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-          <link rel="manifest" href="/site.webmanifest">
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-          <meta name="msapplication-TileColor" content="#da532c">
-          <meta name="theme-color" content="#ffffff"></meta>
-       
-    </head>
     
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
